3ce678a08878c1d04aeaa0dd72178916aab67a75
Muhammad <muhammad@eritheialabs.io>
2025-04-11 18:56:47 +0500

    fix: remove stark custom service

--- app/services/stark/stark_message_service.rb
+++
@@ -1,7 +0,0 @@
-class Stark::StarkMessageService < Stark::BaseService
-  def send_message(question:, session_id:, dealership_id:)
-    session_id = session_id.to_s
-    payload = { question: question, session_id: session_id, dealership_id: dealership_id }
-    send_request(payload)
-  end
-end
