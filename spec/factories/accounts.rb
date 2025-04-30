d4206d009ee205df33de6bb445899d1cd12b8f29
Muhammad <muhammad@eritheialabs.io>
2025-04-30 13:23:19 +0500

    fix: dealersip_id must be uuid

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -3,13 +3,9 @@
 FactoryBot.define do
   factory :account do
     sequence(:name) { |n| "Account #{n}" }
-    sequence(:dealership_id) { SecureRandom.uuid }
     status { 'active' }
     domain { 'test.com' }
     support_email { 'support@test.com' }
-
-    after(:build) do |account|
-      account.extend(Enterprise::Account) if defined?(Enterprise::Account)
-    end
+    dealership_id { SecureRandom.uuid }
   end
 end
