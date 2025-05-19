5c834531a09c054929d9cd2c92c3a39e455ae6ef
Muhammad <muhammad@eritheialabs.io>
2025-05-19 16:53:59 +0500

    feat: after 4 hour window label set to stark

---
+++ app/jobs/schedule_handoff_label_change_job.rb
@@ -0,0 +1,16 @@
+class ScheduleHandoffLabelChangeJob < ApplicationJob
+  queue_as :high
+
+  def perform(conversation)
+    return if conversation.blank?
+
+    handoff_labels = %w[handoff human]
+    current_handoff_label = conversation.labels.find { |label| handoff_labels.include?(label.name) }&.name
+    return unless current_handoff_label
+
+    # Change label to stark after 4 hours
+    conversation.label_list.remove(current_handoff_label)
+    conversation.label_list.add('stark')
+    conversation.update!(last_handoff_at: nil)
+  end
+end
--- app/services/conversation_handoff_service.rb
+++ app/services/conversation_handoff_service.rb
@@ -1,5 +1,6 @@
 class ConversationHandoffService
   HANDOFF_COOLDOWN_HOURS = 4
+  HANDOFF_LABELS = %w[handoff human].freeze
 
   def initialize(conversation)
     @conversation = conversation
@@ -8,7 +9,9 @@ class ConversationHandoffService
   def process_handoff
     return unless should_send_notification?
 
-    @conversation.update(last_handoff_at: Time.current)
+    update_handoff_state
+    schedule_label_change
+
     ConversationHandoff::SendHandoffNotificationsJob.perform_later(@conversation)
   end
 
@@ -19,5 +22,22 @@ class ConversationHandoffService
 
     hours_since_last_handoff = ((Time.current - @conversation.last_handoff_at) / 1.hour).round
     hours_since_last_handoff >= HANDOFF_COOLDOWN_HOURS
+  end
+
+  def update_handoff_state
+    @conversation.label_list.remove('stark')
+
+    # Find which handoff label to use (use existing or first available)
+    current_label = @conversation.labels.find { |label| HANDOFF_LABELS.include?(label.name) }&.name
+    available_label = current_label || HANDOFF_LABELS.first
+
+    @conversation.label_list.add(available_label)
+    @conversation.update!(last_handoff_at: Time.current)
+  end
+
+  def schedule_label_change
+    # Schedule the job to run in exactly 4 hours
+    ScheduleHandoffLabelChangeJob.set(wait: HANDOFF_COOLDOWN_HOURS.hours)
+                                 .perform_later(@conversation)
   end
 end
