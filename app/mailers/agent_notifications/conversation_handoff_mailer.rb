f65348d4189cf49da23bbb8fe379a5fd12b272df
Muhammad <muhammad@eritheialabs.io>
2025-05-22 17:07:32 +0500

    fix: changed email template design & text

--- app/mailers/agent_notifications/conversation_handoff_mailer.rb
+++ app/mailers/agent_notifications/conversation_handoff_mailer.rb
@@ -8,7 +8,7 @@ class AgentNotifications::ConversationHandoffMailer < ApplicationMailer
     @conversation   = conversation
     @action_url     = conversation_url(@conversation)
 
-    subject = "Sales Alert: New Conversation Handoff - #{@account.name} - #{conversation.display_id}"
+    subject = "Sales Alert: New Conversation Handoff - #{@account.name} - #{conversation.inbox.name}"
 
     send_mail_with_liquid(
       to: agents_emails,
