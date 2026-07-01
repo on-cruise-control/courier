4826a7296db0f10a6894e4fbbf4a609c0f57a874
cdeveloper123 <cdeveloper2016@gmail.com>
2026-07-01 18:50:53 +0530

    fix:super admin ui issue fix

--- app/javascript/dashboard/i18n/locale/en/index.js
+++ app/javascript/dashboard/i18n/locale/en/index.js
@@ -31,6 +31,7 @@ import macros from './macros.json';
 import report from './report.json';
 import resetPassword from './resetPassword.json';
 import search from './search.json';
+import sessionLimit from './sessionLimit.json';
 import setNewPassword from './setNewPassword.json';
 import settings from './settings.json';
 import signup from './signup.json';
@@ -78,6 +79,7 @@ export default {
   ...report,
   ...resetPassword,
   ...search,
+  ...sessionLimit,
   ...setNewPassword,
   ...settings,
   ...signup,
